ea67d4a274ccb91023fe7c6419a2e320040379b4
root <6351@holbertonstudents.com>
2023-02-17 15:36:50 -0800

    betty description fix

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -6,8 +6,7 @@
  * prints the sentence "with proper grammar,
  * but the outcome is a piece of art," using
  * the printf function
- */
-
+ */-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
