ee6f8cb2435ab98ea339342fe950fb5490521ff3
root <6351@holbertonstudents.com>
2023-02-17 15:55:20 -0800

    fox

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -15,5 +15,9 @@ int main(void)
 	long int l;
 	long long int ll;
 	float f;
-	printf(sizeof (c),sizeof (i),sizeof (l),sizeof (ll),sizeof (f));
+	printf("Size of a char:" sizeof (c));
+	printf("Size of a int:" sizeof (i));
+	printf("Size of a long int:" sizeof (l));
+	printf("Size of a long long int:" sizeof (ll));
+	printf("Size of a float:" sizeof (f));
 }
