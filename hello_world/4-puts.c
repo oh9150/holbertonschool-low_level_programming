bf4ea04afda9dfb440e9e9bfb694db1ef4e95a72
root <6351@holbertonstudents.com>
2023-02-17 15:24:57 -0800

    puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Prints the text "Programming is like building 
+* a multilingual puzzle" followed by a newline
+* using the function puts.
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle \n");
+	return(0);
+}
