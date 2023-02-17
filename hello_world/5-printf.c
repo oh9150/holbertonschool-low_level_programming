1e6a70a3ac18e1d61e3b03857787902f02577516
root <6351@holbertonstudents.com>
2023-02-17 15:32:17 -0800

    printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/** main - Entry point
+ *
+ * prints the sentence "with proper grammar,
+ * but the outcome is a piece of art," using 
+ * the printf function
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
