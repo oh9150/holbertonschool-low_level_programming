e253f5d6dd84158c906c65d142fb4b0d4695a4f5
root <6351@holbertonstudents.com>
2023-02-17 18:13:29 -0800

    write

---
+++ hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *main - Entry point
+ *
+ * writes the phrase below
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+	return (0);
+}
