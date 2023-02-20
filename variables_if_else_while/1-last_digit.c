8fe2847262d4588e7c00d98e78c12bba6b67f1f7
root <6351@holbertonstudents.com>
2023-02-20 15:39:38 -0800

    space fix but right

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -21,9 +21,9 @@ int main(void)
 	if (n%10 > 5)
 		printf(" and is greater than 5");
 	else if (n%10 < 5)
-		printf("and is less than 6 and not 0");
+		printf(" and is less than 6 and not 0");
 	else
-		printf("and is 0");
+		printf(" and is 0");
 	printf("\n");
 	return (0);
 }
