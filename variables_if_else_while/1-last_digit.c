cbe6dd098978e175a4cd8353a9665a4d8e94ad6d
root <6351@holbertonstudents.com>
2023-02-20 15:33:00 -0800

    compile error

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
 /**
  * main - Entry point
@@ -19,9 +18,9 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf("Last digit of %d is,", n);
-	if n%10 > 5
+	if (n%10 > 5)
 		printf("and is greater than 5");
-	else if n%10 < 5 
+	else if (n%10 < 5)
 		printf("and is less than 6 and not 0");
 	else
 		printf("and is 0");
