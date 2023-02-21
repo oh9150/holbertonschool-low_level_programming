9cd6850d2440ba5d0a0a8c13eb7081f787c27214
root <6351@holbertonstudents.com>
2023-02-20 21:20:45 -0800

    wqtebahpla

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * prints the whole alphabet in one line using
+ * the functions putchar
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c => 'z'; c--)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
