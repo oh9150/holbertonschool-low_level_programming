2d1836108e0f101c0b851fa409a22f2570a41521
root <6351@holbertonstudents.com>
2023-02-20 19:57:32 -0800

    print alphabets

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * prints the alphabet in lowercase followed by
+ * the alphabet in uppercase using the function
+ * putchar followed by a newline
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int c;
+	for(c = 'a'; c => 'z'; c++)
+		putchar(c);
+	for(c = 'A'; c=> 'Z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
