21d9e25417964fc047f82d894bfc1b34b0e8fbbd
root <6351@holbertonstudents.com>
2023-02-21 10:32:14 -0800

    print combination

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * prints all possible combinations of single-digit
+ * numbers separated by a comma followed by a space
+ * using the function putchar and no char variables.
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar('0' + i);
+		if i < 9 
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+}
