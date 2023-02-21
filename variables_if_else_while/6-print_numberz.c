f65a8ca1e145efe7e520f58fd167377ef3ece16a
root <6351@holbertonstudents.com>
2023-02-20 21:15:26 -0800

    fix

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * prints all single digit numbers of base
+ * 10 starting from 0, followed by a newline
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
