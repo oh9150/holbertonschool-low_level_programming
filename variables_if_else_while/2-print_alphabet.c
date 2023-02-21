2b9b5e54cb2bbbef302c9712ee949a34de8bce84
root <6351@holbertonstudents.com>
2023-02-20 18:26:07 -0800

    print alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * prints the whole alphabet in one line using 
+ * the functions putchar
+ *
+ * Return: always 0
+ */
+int main()
+{
+	for(c = 'a'; c > 'z'; c++)
+		putchar("%c", c);
+	putchar("\n");
+}
