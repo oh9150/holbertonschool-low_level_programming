5234dd59f5807d0f44cd01c2f6e47dae8f54c5f3
root <6351@holbertonstudents.com>
2023-02-20 20:00:33 -0800

    betty style

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -12,9 +12,10 @@
 int main(void)
 {
 	int c;
-	for(c = 'a'; c <= 'z'; c++)
+
+	for (c = 'a'; c <= 'z'; c++)
 		putchar(c);
-	for(c = 'A'; c <= 'Z'; c++)
+	for (c = 'A'; c <= 'Z'; c++)
 		putchar(c);
 	putchar('\n');
 	return (0);
