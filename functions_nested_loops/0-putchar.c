14146675539a4168983594a269c56f7a97bf5aea
root <6351@holbertonstudents.com>
2023-02-21 17:30:13 -0800

    forgor to cd

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+
+/**
+ * main - Entry point
+ *
+ * prints the text "_putchar" followed by a newline.
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
