5d490f7197d0ebeeea5249d19a91827f304f1e68
root <6351@holbertonstudents.com>
2023-02-21 17:50:26 -0800

    fix

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,14 +1,13 @@
-#include <main.h>
 /**
  * main - Entry point
  *
- * prints the text "_putchar" followed by a newline.
+ * prints _putchar followed by a newline.
  *
  * Return: always 0.
  */
-int _putchar(char c)
+int main()
 {
-	write(1, &c, 1);
-	write(1, '\n', 1);
-	return (0);
+	char c;
+
+	_putchar(c);
 }
