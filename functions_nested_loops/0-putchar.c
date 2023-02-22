a40a0d172de78b80f62b635fb7b4c2880614992e
root <6351@holbertonstudents.com>
2023-02-21 17:47:16 -0800

    fix

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
-#include <studio.h>
-
+#include <main.h>
 /**
  * main - Entry point
  *
@@ -7,8 +6,9 @@
  *
  * Return: always 0.
  */
-int main(void)
+int _putchar(char c)
 {
-	printf("_putchar");
+	write(1, &c, 1);
+	write(1, '\n', 1);
 	return (0);
 }
