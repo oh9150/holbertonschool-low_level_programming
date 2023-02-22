8af384dff86aa8bc78b5912afa55d28000e9671b
root <6351@holbertonstudents.com>
2023-02-22 15:25:33 -0800

    fix

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,11 +1,10 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - prints the text "_putchar"
+ * followed by a newline.
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: always 0
  */
 int main(void)
 {
