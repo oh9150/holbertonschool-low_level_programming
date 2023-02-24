c4656258c2f887415b9946cebe546b5d8722a4b1
root <6351@holbertonstudents.com>
2023-02-23 19:41:27 -0800

    is lower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - Check description
+ *
+ * Description: Checks whether a character is in lowercase.
+ *
+ * Return: 1 if lowercase, 0 otherwise.
+ */
+int _islower(char c)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if(ch == c)
+			return (1);
+	}
+	return (0);
+}
