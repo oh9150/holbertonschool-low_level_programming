fb32c29bd1d27dddc7639f1f75c2658491d421c7
root <6351@holbertonstudents.com>
2023-02-23 16:21:00 -0800

    fix

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,15 +1,1 @@
-#include "main.h"
 
-/**
- * print_alphabet - prints the alphabet using
- * the function _printchar
- *
- * Return: always 0
- **/
-void print_alphabet(void)
-{
-	char c;
-	for (c = 'a'; c <= 'z'; c++)
-		_putchar(c);
-	_putchar('\n');
-}
