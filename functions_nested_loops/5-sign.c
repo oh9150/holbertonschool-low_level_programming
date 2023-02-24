985c8430c40df77c050fececa64f5e22b2d5a85c
root <6351@holbertonstudents.com>
2023-02-23 20:32:56 -0800

    return NOT returns

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -7,7 +7,7 @@
  * Description: Prints "+" if the sign of the input
  * is positive, "0" if it's 0 and "-" if it's negative.
  *
- * Returns: 1 if n is greater than zero, 0 if it's equal to 0
+ * Return: 1 if n is greater than zero, 0 if it's equal to 0
  * and -1 if it's lower than 0.
  */
 int print_sign(int n)
@@ -20,9 +20,7 @@ int print_sign(int n)
 	{
 		_putchar('-');
 		return (-1);
-	} else
-	{
-		_putchar('0');
-		return (0);
 	}
+	_putchar('0');
+	return (0);
 }
