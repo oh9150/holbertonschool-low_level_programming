241e7da12b16dc27f7f62ab60acab154b66fe103
root <6351@holbertonstudents.com>
2023-02-24 17:36:28 -0800

    times table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * times_table - check description
+ *
+ * Description: prints the times table from 0 to 9.
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int n1;
+	int n2;
+	int n3;
+
+	for (n1 = 0; n <= 9; n++)
+	{
+		for (n2 = 0; n <= 9; n++)
+		{
+			n3 = n2 * n1;
+			write(1, &n3, sizeof(n3));
+			write(1, ", ", 2);
+		}
+		_putchar('\n');
+	}
+}
