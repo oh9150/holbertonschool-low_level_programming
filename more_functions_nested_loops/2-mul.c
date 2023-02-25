8f552372ee1baf5af8b2b8ab7094706dd3642ba8
root <6351@holbertonstudents.com>
2023-02-24 20:04:58 -0800

    if this is it thats too easy

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - check description
+ * @a: int #1
+ * @b: int #2
+ *
+ * Description: multiplies two integers.
+ *
+ * Return: the multiplication of a and b.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
