1a57f76545267b16f0876e9dd8d6edf7bab61c6e
root <6351@holbertonstudents.com>
2023-02-24 19:18:12 -0800

    fix

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -18,8 +18,11 @@ void times_table(void)
 		for (n2 = 0; n2 <= 9; n2++)
 		{
 			n3 = n2 * n1;
-			if (n3 > 9 && n2 != 0)
+			if (n3 > 9)
+			{
+				if (n2 != 0)
 				_putchar('0' + (n3 - (n3 % 10)) / 10);
+			}
 			else
 				_putchar(' ');
 			_putchar('0' + (n3 % 10));
