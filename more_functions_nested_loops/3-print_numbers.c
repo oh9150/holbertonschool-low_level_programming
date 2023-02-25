2d0e482eef1d86030f83968a62c9e0e0649ad5be
root <6351@holbertonstudents.com>
2023-02-24 20:07:42 -0800

    print numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - check description
+ *
+ * Description: prints the numbers 0-9 followed by a newline
+ * using putchar twice.
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		_putchar(i);
+	_putchar('\n');
+}
