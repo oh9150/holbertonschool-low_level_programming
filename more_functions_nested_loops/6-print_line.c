f6d1672994297ab2f328276a9510cca0e8884243
root <6351@holbertonstudents.com>
2023-02-24 21:34:22 -0800

    print line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - check description
+ * @n: the length of the line.
+ *
+ * Description: prints a line of length n.
+ *
+ * Return: void.
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
+
