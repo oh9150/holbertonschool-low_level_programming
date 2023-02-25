969de58db2914a2486f3432d82ac0b77d47ecdc0
root <6351@holbertonstudents.com>
2023-02-24 21:45:56 -0800

    print diagonal

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - check description.
+ * @n: the length of the diagonal.
+ *
+ * Description: prints a diagonal of length n.
+ *
+ * Return: void.
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\');
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
