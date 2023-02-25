ab440d90e7ab3477796a5eeff8c9b97bcf1dcb2c
root <6351@holbertonstudents.com>
2023-02-24 17:03:38 -0800

    print minutes

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * jack_bauer - check description.
+ *
+ * Description: prints all the minutes in a day.
+ *
+ * Return: void.
+ */
+void jack_bauer(void)
+{
+	int h1;
+	int ;
+
+	for (i = 0; i <= 23; i++)
+	{
+		for (j = 0; j <= 59; j++)
+		{
+			if (i < 10)
+			{
+				write(1, '0', 
