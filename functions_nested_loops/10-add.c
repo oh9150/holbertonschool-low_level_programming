a154a5e943da828bd8225bbf5464d65caaf3bd43
root <6351@holbertonstudents.com>
2023-02-24 17:38:44 -0800

    add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - check description
+ * @a: int #1
+ * @b: int #2
+ *
+ * Description: returns the sum of int a and b.
+ *
+ * Return: The sum of a and b.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
