93bb23d61930101ff3745af7a7c576325cb62c57
root <6351@holbertonstudents.com>
2023-02-24 21:56:07 -0800

    square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - check description
+ * @size: the size of the square
+ *
+ * Description: prints a square of side length size.
+ *
+ * Return: void.
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	} else
+		_putchar('\n');
+}
