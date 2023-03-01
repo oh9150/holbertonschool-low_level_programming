7f45d37639f73afabf6951a682d023e61c0de221
root <6351@holbertonstudents.com>
2023-02-28 19:57:21 -0800

    fix

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -13,16 +13,11 @@ void puts_half(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\n'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{}
-	_putchar('0' + i);
 	if (i % 2 == 1)
 		i--;
 	i = i / 2;
-	_putchar('0' + i);
 	for (; str[i] != '\0'; i++)
-	{
 		_putchar(str[i]);
-		_putchar('a');
-	}
 }
