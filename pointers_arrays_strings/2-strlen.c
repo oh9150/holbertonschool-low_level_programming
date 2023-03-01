a0be5133bbe87f97e25c4b042e6d15beb01ae9fd
root <6351@holbertonstudents.com>
2023-02-28 16:50:30 -0800

    strlen

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - check description
+ * @s: the string to check.
+ *
+ * Description: returns the length of s.
+ *
+ * Return: the length of s.
+ */
+int _strlen(char *s)
+{
+	return (sizeof(s));
+}
