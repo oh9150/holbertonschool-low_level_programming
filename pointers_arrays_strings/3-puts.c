47a5da32405ac6f074e3f42148ba14f4c4df0445
root <6351@holbertonstudents.com>
2023-02-28 18:42:40 -0800

    puts

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - check description
+ * @str: the string to print
+ *
+ * Description: prints the string str.
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] =! '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+}
