50a82e02cfa74d3fccf01a4c487d8a74e7aa37e5
root <6351@holbertonstudents.com>
2023-02-28 19:49:27 -0800

    put half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts_half - check description
+ * @str: the string to print.
+ *
+ * Description: prints the second half of the string
+ * str.
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\n'; i++)
+	{}
+	if (i % 2 == 1)
+		i--;
+	i = i / 2
+	for (; str[i] != '\0'; i++)
+		_putchar(str[i]);
+}
