9e017543c9772e2ab4cf3df4b292a94d9c4c3d8c
root <6351@holbertonstudents.com>
2023-02-28 16:43:51 -0800

    swap

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - check description
+ * @a: int #1
+ * @b: int #2
+ *
+ * Description: swaps the value of a and b.
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int aa;
+
+	aa = a;
+	*a = b;
+	*b = aa;
