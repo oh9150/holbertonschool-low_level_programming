2af77960942ad4ca58ec9e0aa4ada2227ec7949d
root <6351@holbertonstudents.com>
2023-02-28 16:38:03 -0800

    reset to 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - check description
+ * @c: pointer of a variable
+ *
+ * Description: set the value of the variable c
+ * is pointing to to 98.
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
