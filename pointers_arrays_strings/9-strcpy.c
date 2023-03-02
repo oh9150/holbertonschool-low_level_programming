2e42ffb0eb21c4e2bf7378042eb01109cfb69e2e
root <6351@holbertonstudents.com>
2023-03-01 16:15:33 -0800

    string copy

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcpy - read description
+ * @src: the string to copy from
+ * @dest: the string to copy to
+ *
+ * Description: copies string src into string dest.
+ *
+ * Return: pointer to dest 
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; i != '\0'; i++)
+		dest[i] = src[i];
+	return (*dest);
+}
