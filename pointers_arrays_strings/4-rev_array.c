d904abafd70b14be5cc2bf1a8384deea4ff5c209
root <6351@holbertonstudents.com>
2023-03-02 18:07:42 -0800

    reverse array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - check description
+ * @a: the array to reverse
+ * @n: the number of elements in a
+ *
+ * Description: reverses the content of a
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int *n)
+{
+	int b[n];
+	int i;
+
+	for (i = 0; i < n; i++)
+		b[i] = a[i];
+	for (i = 0; i < n; i++)
+		a[i] = b[n - i];
+}
