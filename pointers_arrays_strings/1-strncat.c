da9b365d61da2335184aa6d368e65dd80e8a8dc1
root <6351@holbertonstudents.com>
2023-03-02 17:20:13 -0800

    strncat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncat - check description
+ * @dest: the string that src will append to
+ * @src: the string that will append to dest
+ * @n: the max number of values appended to dest from src
+ *
+ * Description: appends n characters from src to dest.
+ *
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{}
+	for (j = i; src[j - i] != '\0'; j++)
+		if (j - i <= n)
+		{
+			dest[j] = src[j - i];
+		}
+
+	return (dest);
+}
