d7ae84543671e328627e7aacac886d7fc78f8799
root <6351@holbertonstudents.com>
2023-03-02 17:37:16 -0800

    i forgor main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -21,3 +21,7 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
 char *_strcat(char *dest, char *src);
+
+char *_strncpy(char *dest, char *src, int n);
+
+int _strcmp(char *s1, char *s2);
