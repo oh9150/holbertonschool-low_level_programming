7f61b6b981eee7e13093e0521aeb1cde077032a2
root <6351@holbertonstudents.com>
2023-03-02 16:47:16 -0800

    strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * strcat - check description
+ * @dest: the string that src will append to
+ * @src: the string that will append to dest
+ *
+ * Description: appends src to dest, so that the text in src
+ * is before the one in dest.
+ *
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{}
+	i--
+	for (j = i; dest[j - i] != '\0'; j++)
+		src[j] = dest[j - i];
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+}
+
