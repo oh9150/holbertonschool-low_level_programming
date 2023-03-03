d4e8fda5af99799bd79afc6cdbb673c74591e93c
root <6351@holbertonstudents.com>
2023-03-02 17:44:53 -0800

    string compare

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcmp - check description
+ * @s1: first string to compare
+ * @s2: second string to compare
+ *
+ * Description: compares s1 to s2.
+ *
+ * Return: -1 if s1 is lower than s2, 0 if it's equal to s2
+ * and 1 if it's greater than s2
+ */
+int _strcmp(char *s1 *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] <= '0'; i++)
+	{
+		if (s1[i] < s2[i])
+			return (-1);
+		else if (s1[i] > s2[i])
+			return (1);
+	}
+
+	return (0);
+}
