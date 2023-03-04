b1394f42d04aa0b397ca3fe5eb57505800b395f7
root <6351@holbertonstudents.com>
2023-03-03 20:51:47 -0800

    bools dont exist??

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -12,20 +12,20 @@ char *string_toupper(char *s)
 {
 	int i = 0;
 	int j;
-	bool afterseparator = true;
+	int afterseparator = 1;
 	char separators[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'}
 
 	while (s[i] != '\0')
 	{
-		if (s[i] <= 'z' && s[i] >= 'a' && afterseparator == true)
+		if (s[i] <= 'z' && s[i] >= 'a' && afterseparator)
 			s[i] = s[i] + 32;
 		i++;
-		afterseparator = false;
+		afterseparator = 0;
 		for (j = 0; j < 13; j++)
 		{
 			if (separators[j] = i)
 			{
-				afterseparator = true;
+				afterseparator = 1;
 				
 				break;
 			}
