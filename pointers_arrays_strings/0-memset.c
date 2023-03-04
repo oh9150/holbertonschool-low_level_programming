9704fc0709e063235297709f5b09cc6e5c28a8f3
root <6351@holbertonstudents.com>
2023-03-04 14:47:10 -0800

    fix

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -12,12 +12,14 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
+	int *mem = s;
+	int i = 0;
 
 	while (i < n)
 	{
-		*s + i = b;
-		i++;
+		*s = b;
+		s++;
+		i++
 	}
-	return (*s);
+	return (mem);
 }
