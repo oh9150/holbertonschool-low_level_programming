ba4c0478d134e9bc67090b695b934c872a45421a
root <6351@holbertonstudents.com>
2023-03-03 20:27:22 -0800

    string to upper

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - check description
+ * @s: the string to uppercase.
+ *
+ * Description: uppercases all letters in s.
+ *
+ * Return: the string in uppercase
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] <= 'z' && s[i] >= 'a')
+			s[i] = s[i] + 32;
+	}
+}
