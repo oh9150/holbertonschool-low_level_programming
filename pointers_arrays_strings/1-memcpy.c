1440569a74d6198adf885a10119a036de2790a79
root <6351@holbertonstudents.com>
2023-03-05 14:28:20 -0800

    mem copy

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * memcpy - check description
+ * @dest: the area to copy to
+ * @src: the area to copy from
+ * @n: the ammount of bytes to copy
+ *
+ * Description: copies n ammount of bytes from the memory
+ * area src to the memory area dest.
+ *
+ * Return: the pointer to dest.
+ */
+char _memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	while (n--)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
