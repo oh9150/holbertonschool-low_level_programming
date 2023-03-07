1af1870e4242c893d9a99752fd929636c1cd5503
root <6351@holbertonstudents.com>
2023-03-06 17:32:40 -0800

    fix

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - check description
+ * @s: the string to check
+ *
+ * Description: finds the length of s using recursion
+ *
+ * Return: the length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	return (1 + _strlen_recursion(*s + 1));
+}
