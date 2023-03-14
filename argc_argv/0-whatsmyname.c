67e4bc2feb32ffb3f0683f4820322c28ff68167e
root <6351@holbertonstudents.com>
2023-03-13 20:23:38 -0700

    fixes

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -5,9 +5,10 @@
  * Description: prints the name of the file, followed
  * by a newline.
  *
- * Return: void
+ * Return: 0
  */
-void main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
+	return (0);
 }
