5c812584e7407773693b2e3bd66f20a910d02cf6
root <6351@holbertonstudents.com>
2023-03-13 20:48:32 -0700

    multiplication

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: the size of the array argv
+ * @argv: an array containing all of the arguments passed
+ * by the compiler
+ *
+ * Description: prints the result of the multiplication of two numbers,
+ * if less than 3 arguments are passed, prints "Error"
+ *
+ * Return: if there are 3 parameters passed, 0; if less than 3, 1
+ */
+int main(int argc, char *argv[])
+{
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", argv[1] * argv[2]);
+	return (0);
+}
