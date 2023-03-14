ade3a88cb48e4f0c059c35f5074feb45e815c065
root <6351@holbertonstudents.com>
2023-03-13 20:35:45 -0700

    print argc

---
+++ argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: the ammount of items in argv
+ * @argv: an array which contains all the arguments passed
+ * through the compiler
+ *
+ * Description: prints the number of arguments passed into main,
+ * followed by a newline
+ *
+ * Return: 0
+ */
+int main(int argc, __unused__((argument))char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+
+}
