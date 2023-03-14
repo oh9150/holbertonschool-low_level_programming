c8b338f86959765f9a88f1178aff092aa3a74a78
root <6351@holbertonstudents.com>
2023-03-13 20:19:07 -0700

    whats my name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints the name of the file, followed
+ * by a newline.
+ *
+ * Return: void
+ */
+void main(argc, argv[])
+{
+	printf("%s\n", argv[0]);
+}
