77b7855323d37db7519ef71a5654fc80a273bb5a
root <6351@holbertonstudents.com>
2023-03-13 21:16:33 -0700

    add

---
+++ argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: the ammount of items in argv
+ * @argv: an array containing all the items passed through the compiler
+ *
+ * Descrion: if only numbers are passed as arguments, sums them;
+ * if one of the number contains symbols that are not digits, print Error;
+ * and if no numbers are passed, prints 0
+ *
+ * Return: 0 unless one of the number contains symbols that are not digits
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return(0);
+	}
+	argc--;
+	for (; argc >= 1; argc--;)
+	{
+		if (atoi(argv[argc]) != 0)
+			sum += argv[argc];
+		else
+		{
+			printf("Error");
+			return (0);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
