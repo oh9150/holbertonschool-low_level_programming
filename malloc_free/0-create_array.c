6492f46e14913ea67d837f57f3bf9975ee87c673
root <6351@holbertonstudents.com>
2023-03-15 19:40:10 -0700

    last one

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -15,13 +15,17 @@
 char *create_array(unsigned int size, char c)
 {
 	char *ptr;
+	int i;
 
 	if (size == 0)
 		return ('\0');
 
 	ptr = (char *)malloc(sizeof(char) * size);
 
-	for (; size > 0; size = size - sizeof(char))
+	while (i <= size)
+	{
 		ptr[size] = c;
+		i += sizeof(char);
+	}
 	return (ptr);
 }
