94405996ea7c159edc5892ed5b16c3b5edbec457
root <6351@holbertonstudents.com>
2023-03-15 20:53:40 -0700

    string dupe

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - check description
+ * @str: the string to copy
+ *
+ * Description: allocates memory and copies the string [str]
+ * into that memory
+ *
+ * Return: a pointer to the duplicated string, or null if string
+ * is null, or null if memory was insufficient.
+ */
+char *_strdup(char *str)
+{
+	int len = 0;
+	char *s;
+
+	if (*str == NULL)
+		return (NULL);
+
+	while (*str)
+		len++;
+
+	s = (char *)malloc(len * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+
+	while (len >= 0)
+	{
+		s[len] = *str;
+		*str++;
+		len--;
+	}
+	return (s);
+}
