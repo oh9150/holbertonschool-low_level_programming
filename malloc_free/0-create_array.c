f1cca35b8557d21e79a293b788a1aace482a4e55
root <6351@holbertonstudents.com>
2023-03-15 19:57:15 -0700

    fix

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -22,8 +22,8 @@ char *create_array(unsigned int size, char c)
 		return ('\0');
 
 	ptr = (char *)malloc(sizeof(char) * size);
-
-	if (ptr == '\0')
+	
+	if (ptr == NULL)
 		return ('\0');
 
 	while (i <= size)
