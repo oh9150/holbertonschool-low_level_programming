6211fe1a53c6ee01e71ec4b34fcaabfda547dd0c
root <6351@holbertonstudents.com>
2023-03-15 19:10:02 -0700

    create array

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_array - check description
+ * @size: the size of the array of chars to allocate
+ * @c: the char to initialize the memory with
+ *
+ * Description: creates an array of chars of size [size],
+ * and initializes it with a the char [c]
+ *
+ * Return: a pointer to the first byte of the array or
+ * NULL if size <= 0 or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+
+	if (size == 0)
+		return ('\0');
+
+	ptr = malloc(sizeof (char) * size);
+
+	if (*ptr == '\0')
+		return ('\0');
+
+	for (; size <= 0; size--)
+		*ptr[size] = c;
+	return (ptr)
+}
