52cb876f65e6266720bfec4b0da5771c751a2bb1
root <6351@holbertonstudents.com>
2023-03-16 19:55:57 -0700

    allocate gri

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - allocates memory for a
+ * two-dimensional array and initiallizes all
+ * elements to '0'
+ * @width: the width of the array
+ * @height: the height of the array
+ *
+ * Return: a pointer to the array or NULL if it fails to allocate
+ * or NULL if [width] or [height] <= 0
+ */
+int **alloc_grid(int width, int height)
+{
+	int **array;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	array = (int **)malloc(sizeof(int *) * width);
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; i < width; i++)
+		array[i] = (int *)malloc(sizeof(int) * height);
+	for (i = 0; i < width; i++)
+	{
+		for (j = 0; j < height; j++)
+			array[i][j] = 0;
+	}
+	return (array);
+}
