26a6442a1791d3f764c685823ef203625e1e1f69
root <6351@holbertonstudents.com>
2023-03-16 19:05:00 -0700

    fix

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -7,54 +7,46 @@
  * @s2: the second string to concatenate
  *
  * Description: concatenates [s1] with [s2], if either of them
- * are NULL it's treated as an empty string and stores it into a new space in memory
+ * are NULL it's treated as an empty string and stores it into
+ * a new space in memory
  *
  * Return: a pointer to the new string, or NULL if not enough memory
  */
 char *str_concat(char *s1, char *s2)
 {
-	int len1, len2 = 0;
+	int len1, len2, i = 0;
 	char *new_str, *ptr1, *ptr2;
-	int i = 0;
 
 	ptr1 = s1;
 	ptr2 = s2;
-	if (s1)
+	if (!(s1))
+		s1 = "";
+	while (*s1)
 	{
-		while (*s1)
-		{
-			len1++;
-			s1++;
-		}
+		len1++;
+		s1++;
 	}
-
-	if (s2)
+	if (!(s2))
+		s2 = "";
+	while (*s2)
 	{
-		while (*s2)
-		{
-			len2++;
-			s2++;
-		}
+		len2++;
+		s2++;
 	}
-
 	new_str = (char *)malloc((len1 + len2 + 1) * sizeof(char));
 	if (!(new_str))
 		return (NULL);
 
-	if (s1 || s2)
+	for (i = 0; i <= (len1 + len2); i++)
 	{
-		while(len1 + len2 >= i)
+		if (i > len1)
 		{
-			if (len1 > i)
-			{
-				new_str[i] = *ptr1;
-				ptr1++;
-			}else 
-			{
-				new_str[i] = *ptr2;
-				ptr1++;
-			}
-			i++;
+			new_str[i] = *ptr1;
+			ptr1++;
+		} else
+		{
+			new_str[i] = *ptr2;
+			ptr2++;
 		}
 	}
 	new_str[i] = '\0';
