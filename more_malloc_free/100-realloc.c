b4140f2278bea5f555960e87293f0098f49e1a7f
root <6351@holbertonstudents.com>
2023-03-18 23:30:12 -0700

    fix i hope

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -36,7 +36,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size > old_size)
 		new_size = old_size;
 	for (i = 0; i < new_size; i++)
-		new_mem[i] = ptr[i];
+		new_mem[i] = new_ptr[i];
 	free(ptr);
 	return (new_mem);
 }
