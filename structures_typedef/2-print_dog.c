6c804d96b0e14802dbc983168869c711a25de6c1
root <6351@holbertonstudents.com>
2023-03-22 16:07:08 -0700

    syntax error

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -20,5 +20,5 @@ void print_dog(struct dog *d)
         if (d->owner != NULL)
                 printf("Owner: (nil)\n");
         else
-                printf("Owner: %s\n", d->owneri);
+                printf("Owner: %s\n", d->owner);
 }
