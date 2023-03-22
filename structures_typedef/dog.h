670cf68d36f2124a3f935030a78f44bf7d69914d
root <6351@holbertonstudents.com>
2023-03-22 15:37:04 -0700

    double inclusion fix + documentation

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,15 @@
+#ifndef DOG_STRUCT
+#define DOG_STRUCT
+/**
+ * dog - dog
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the dog's owner
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
