607a7db1a6ddf15e450c25f50802dadc5a821c95
root <6351@holbertonstudents.com>
2023-03-23 18:34:49 -0700

    calc

---
+++ function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: the ammount of arguments
+ * @argv: an array of arguments passed by the compiler
+ *
+ * Description: atois the integers passed and calls get_op_func and prints the
+ * result
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int (*function)(int, int);
+	int result;
+
+	if (argc != 4)
+	{
+		printf("Error");
+		exit(98);
+	}
+	function = get_op_func(argv[2]);
+	if (function == NULL)
+	{
+		printf("Error");
+		exit(99);
+	}
+
+	result = function(atoi(argv[1]), atoi(argv[3]));
+	printf("%d", result);
+
+	return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,67 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * op_add - adds the two integers
+ *
+ * Return: the sum of the two integers
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts [b] from [a]
+ * @a: the value to subtract to
+ * @b: the value to subtract from [a]
+ *
+ * Return: the result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies the two integers
+ * @a: int #1
+ * @b: int #2
+ *
+ * Return: the result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides [a] / [b], if b == 0, prints error and exits with code 100
+ * @a: an int
+ * @b: an int
+ *
+ * Return: the result
+ */
+int op_div(int a, int b)
+{
+	if (b != 0)
+		return (a / b);
+	printf("Error");
+	exit(100);
+}
+
+/**
+ * op_mod - finds [a] % [b], if b == 0, prints error and exits with code 100
+ * @a: an int
+ * @b: an int
+ *
+ * Return: the result
+ */
+ int op_mod(int a, int b)
+{
+	if (b != 0)
+		return (a % b);
+	printf("Error");
+	exiy(100);
+}
