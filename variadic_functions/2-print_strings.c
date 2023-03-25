a3a7c9482ecc3506769e077269e94919e322732e
root <6351@holbertonstudents.com>
2023-03-24 19:22:34 -0700

    fix

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -18,7 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		string = va_arg(args, char *)
+		string = va_arg(args, char *);
 		if (string != NULL)
 		{
 			printf("%s", string);
