5d22a7b55ceeab411c058ccbfd38fd6ab630c39d
root <6351@holbertonstudents.com>
2023-03-24 20:17:40 -0700

    last one git add .git add . til it compiles..

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,7 +1,7 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
-
+#include <string.h>
 /**
  * print_all - prints anything
  * @format: a string containing the types of each parameter, (one letter for
@@ -15,9 +15,9 @@ void print_all(const char * const format, ...)
 	char *types = "cifs";
 	va_list args;
 	char *str;
-	int len = sizeof(format);
+	int len = strlen(format);
 
-	va_start(args, len);
+	va_start(args, format);
 	while (*format && *(format + i))
 	{
 		while (*(types + j) == *(format + i))
