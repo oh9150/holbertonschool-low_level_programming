f0c031bb81035f7c818e36e3f0fce8f70f0d1a91
root <6351@holbertonstudents.com>
2023-03-24 21:49:10 -0700

    remove start variable

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,17 +1,14 @@
-section .data
-hello: db "Hello, World", 15
-helloLen: equ $-hello
-
-section .text
-global _start
-
 _start:
-mov rax,1
-mov rdi,1
-mov rsi,hello
-mov rdx,helloLen
-syscall
+	mov rax,1
+	mov rdi,1
+	mov rsi,hello
+	mov rdx,helloLen
+	syscall
 
-mov rax,60
-mov rdi,0
-syscall
+	mov rax,60
+	mov rdi,0
+	syscall
+
+section .data
+	hello: db "Hello, World", 0xA
+	helloLen: equ $-hello
