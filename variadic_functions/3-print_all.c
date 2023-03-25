7122c0f1869371a7a9a6a652b1ecd9bd0334e96e
root <6351@holbertonstudents.com>
2023-03-24 20:09:45 -0700

    print all

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,56 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+ * print_all - prints anything
+ * @format: a string containing the types of each parameter, (one letter for
+ * each: c for char, i for int, , f for float, and s for string (char *))
+ *
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0, j = 0;
+	char *types = "cifs";
+	va_list args;
+	char *str;
+	int len = strlen(format);
+
+	va_start(args, len);
+	while (*format && *(format + i))
+	{
+		while (*(types + j) == *(format + i))
+		{
+			if (i < len - 1)
+			{
+				printf(", ");
+				break;
+			} j++;
+		} j = 0;
+		switch(types[i])
+		{
+			case 'c':
+				printf("%c", va_arg(args, char));
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, float));
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (str)
+				{
+					printf("%s", str);
+					break;
+				}
+				printf("(nil)");
+				break;
+		} i++;
+	}
+	printf("\n");
+	va_end(args);
+}
