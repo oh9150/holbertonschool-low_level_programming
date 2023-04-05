93c3b8623339afe9760289877e34bb55c4ce3f38
root <6351@holbertonstudents.com>
2023-04-04 20:53:04 -0700

    fix

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,9 +12,9 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_head = malloc(sizeof(list_t));
 	int len;
 
-	for (len = 0; *(str + len * sizeof(char)) == '\0'; len++)
+	for (len = 0; *(str + len * sizeof(char)) != '\0'; len++)
 		;
-	len++;
+
 	if (new_head == NULL)
 	{
 		free(new_head);
