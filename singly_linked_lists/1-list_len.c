6cccf6a2e4c5a8f58f6a20f5bdc75fcbe03ef207
root <6351@holbertonstudents.com>
2023-04-04 20:02:40 -0700

    list lenght

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a list
+ * @h: a pointer to the head of the list
+ *
+ * Return: the number of elements in a list
+ */
+size_t list_len(list_t *h)
+{
+	int i = 0;
+	int *current = h;
+
+	while (*current != NULL)
+	{
+		current = current->next;
+		i++;
+	}
+	return (i);
+}
