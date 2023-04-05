0738d715f8927d2f967efd5fa77128f6070ef9e5
root <6351@holbertonstudents.com>
2023-04-04 20:43:19 -0700

    fix

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -5,22 +5,33 @@
  * @head: a pointer to the head of the list
  * @str: the string to copy
  *
- * Return: a pointer to the new head of the list
+ * Return: a pointer to the new head of the list, NULL if it fails
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_head = malloc(sizeof(list_t));
+	int len;
+
+	for (i = 0; *(str + i * sizeof(char)) == '\0'; i++)
+		;
+
 	if (new_head == NULL)
 	{
 		free(new_head);
 		return (NULL);
 	}
-	new_head->next = *head;
 	new_head->str = strdup(str);
+	new_head->len = len;
 	if (new_head->str == NULL)
 	{
-		free (new_head);
+		free(new_head);
 		return (NULL);
 	}
+	if (head == NULL)
+		new_node->next = NULL;
+	else
+		new_node->next = *head;
+
+
 	return (new_head);
 }
