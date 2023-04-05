1cc82c5bf31a49671a4fe4bd10cb3b8b5195b8a3
root <6351@holbertonstudents.com>
2023-04-04 20:26:28 -0700

    add node

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a node at the beggining of a linked list
+ * @head: a pointer to the head of the list
+ * @str: the string to copy
+ *
+ * Return: a pointer to the new head of the list
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_head = malloc(sizeof(list_t));
+	if (new_head == NULL)
+		return (NULL);
+	new_head->next = *head;
+	new_head->str = strdup(str);
+	return (new_head);
+}
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -20,7 +21,7 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(list_t *h);
-
 size_t list_len(list_t *h);
+list_t *add_node(list_t **head, const char *str);
 
 #endif
