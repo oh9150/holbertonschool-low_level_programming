7f2079b6d47fb9047c93d77bbfe1a247265124e9
root <6351@holbertonstudents.com>
2023-04-05 23:31:05 -0700

    before main

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * startup - prints "You're beat! and yet, you must allow,\nI bore my
+ * house upon my back!\n" before the main function is executed
+ * Return: void
+ */
+void startup(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
