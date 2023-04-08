ffb90d8b70641f50bed4eb9c8653b83dc29fabc5
root <6351@holbertonstudents.com>
2023-04-08 02:44:04 -0700

    fix

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,14 +1,17 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints all the elements in a doubly linked list
+ * dlistint_len - returns the ammount of nodes in the list
  * @h: a node of the list
  *
  * Return: the ammount of nodes in the list
  */
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t i = 0;
+
+	if (!h)
+		return (0);
 
 	while (h->prev != NULL)
 		h = h->prev;
