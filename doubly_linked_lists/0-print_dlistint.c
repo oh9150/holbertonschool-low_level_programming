13a2fef5cfff64a69902f498bb1c82ba1038241a
root <6351@holbertonstudents.com>
2023-04-08 02:33:30 -0700

    fix

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -8,11 +8,11 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t i;
+	size_t i = 0;
 
 	while (h->prev != NULL)
 		h = h->prev;
-	while (h->next != NULL)
+	while (h)
 	{
 		h = h->next;
 		i++;
