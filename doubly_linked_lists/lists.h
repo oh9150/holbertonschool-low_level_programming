519eae964b2a31b5f8562abdc250f1613ed74faf
root <6351@holbertonstudents.com>
2023-04-07 18:22:33 -0700

    fix

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -18,9 +18,9 @@ size_t print_dlistint(const dlistint_t *h);
  */
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 #endif
