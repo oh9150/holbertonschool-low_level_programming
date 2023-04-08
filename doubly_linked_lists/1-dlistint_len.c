941cf8369e324a426283f1dd09dc9d709a98be0c
root <6351@holbertonstudents.com>
2023-04-08 02:26:32 -0700

    fix

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements in a doubly linked list
+ * @h: a node of the list
+ *
+ * Return: the ammount of nodes in the list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h->prev != NULL)
+		h = h->prev;
+	while (h->next != NULL)
+	{
+		h = h->next;
+		i++;
+		printf("%d", h->n);
+	}
+	return (i);
+}
