c1f9fd09803f712e0deaad8d81afa4b479c9c666
root <6351@holbertonstudents.com>
2023-04-13 16:57:59 -0700

    read textfile

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a node at the beggining of a linked list
+ * @head: a pointer to the head of the list
+ * @str: the string to copy
+ *
+ * Return: a pointer to the new head of the list, NULL if it fails
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_head = malloc(sizeof(list_t));
+	int len;
+
+	for (len = 0; *(str + len * sizeof(char)) != '\0'; len++)
+		;
+
+	if (new_head == NULL)
+	{
+		free(new_head);
+		return (NULL);
+	}
+	new_head->str = strdup(str);
+	new_head->len = len;
+	if (new_head->str == NULL)
+	{
+		free(new_head);
+		return (NULL);
+	}
+	while (*head->prev)
+		head = *head->prev
+	if (head == NULL)
+		new_head->next = NULL;
+	else
+		new_head->next = *head;
+	*head = new_head;
+
+	return (new_head);
+}
---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a file and prints it to standard output
+ * @filename: the name of the file
+ * @letters: the ammount of letters to read
+ *
+ * Return: the actual number of letters it could print, 0 if the file
+ * cannot be opened or read or if write fails
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t read, wrote;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open (filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+	{
+		free(buf);
+		return (0);
+	}
+	read = read(fd, buf, letters);
+	if (read == -1)
+	{
+		close(fd);
+		free(buf);
+		return (0);
+	}
+	wrote = write(STOUT_FILENO, buf, read);
+	if (wrote == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+	close(fd);
+	return(wrote);
