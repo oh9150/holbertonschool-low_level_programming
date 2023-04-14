d829dd2a59e098037eaac205c9f257a14c115a9e
root <6351@holbertonstudents.com>
2023-04-13 20:28:54 -0700

    text_content NULL case

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -17,6 +17,11 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (ERROR_VALUE);
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (ERROR_VALUE);
+	}
 
 	write_result = write(fd, text_content, strlen(text_content));
 	if (write_result == -1)
