b8ec42c980f6cc895889d078feb8e11b15091c99
root <6351@holbertonstudents.com>
2023-04-13 20:34:03 -0700

    appent to text file

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: the name of the file
+ * @text_content: the text to write in the file
+ *
+ * Return: 1 on success, -1 otherwise
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, write_result, ERROR_VALUE = -1, SUCCESS_VALUE = 1;
+
+	if (!filename)
+		return (ERROR_VALUE);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_APPEND, 0600);
+	if (fd == -1)
+		return (ERROR_VALUE);
+
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (SUCCESS_VALUE);
+	}
+	write_result = write(fd, text_content, strlen(text_content));
+	if (write_result == -1)
+	{
+		close(fd);
+		return (SUCCESS_VALUE);
+	}
+	close(fd);
+	return (SUCCESS_VALUE);
+}
