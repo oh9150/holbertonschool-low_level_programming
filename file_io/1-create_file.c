e9f9d1b4cc4888c633e2ba8e5f5a29041330834e
root <6351@holbertonstudents.com>
2023-04-13 20:32:48 -0700

    fix...

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,10 +14,15 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (ERROR_VALUE);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (ERROR_VALUE);
 
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (SUCCESS_VALUE);
+	}
 	write_result = write(fd, text_content, strlen(text_content));
 	if (write_result == -1)
 	{
