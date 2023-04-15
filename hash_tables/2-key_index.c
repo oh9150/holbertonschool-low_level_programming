299f5cbe23948aee8f22805dc7429fbf0969b7ce
root <6351@holbertonstudents.com>
2023-04-14 22:13:36 -0700

    key index

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gives the index of a key
+ * @key: the key to search
+ * @size: the size of the array
+ *
+ * Return: the index of the key
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	size_t index;
+
+	if (size == 0)
+		return (0);
+
+	index = hash_djb2(key);
+
+	return (key);
+}
