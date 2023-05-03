f32a421fca9cfde679bf4994c4df8e7f91f3130f
root <6351@holbertonstudents.com>
2023-05-02 18:16:17 -0700

    fix

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -9,7 +9,7 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	size_t index = key_index(key);
+	size_t index = key_index(key, ht->size);
 	hash_node_t *new_node;
 
 	if (!(ht | key | value))
