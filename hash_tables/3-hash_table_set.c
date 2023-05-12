5f1da7a92d1ef869c7b911419958cd9b9ae0ed37
root <6351@holbertonstudents.com>
2023-05-11 21:11:58 -0700

    fix

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -7,16 +7,19 @@
  *
  * Return: 1, 0 on fail
  */
-int hash_table_set(hash_table_t *ht, const unsigned char *key, const char *value)
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	size_t index = key_index(key, ht->size);
 	hash_node_t *new_node;
-	array = ht->array;
+	hash_node_t **array = ht->array;
 
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
 		return (0);
 
+	new_node->key = strdup(key);
+	if (new_node->key)
+		return (0);
 	new_node->value = strdup(value);
 	if (new_node->value)
 		return (0);
@@ -24,7 +27,7 @@ int hash_table_set(hash_table_t *ht, const unsigned char *key, const char *value
 	if (*array[index])
 		new_node->next = *array[index];
 
-	ht->*array[index] = new_node;
+	*array[index] = new_node;
 		
 	return (1);
 }
