6a2ba78075be7073deb86167a14a62725857e0a8
root <6351@holbertonstudents.com>
2023-05-11 19:24:04 -0700

    fix...

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -7,7 +7,7 @@
  *
  * Return: a pointer to the new head of the list, NULL if it fails
  */
-dlistint_t *add_node(dlistint_t **head, const int n)
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
 
