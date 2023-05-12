4bc05040c610bc7d1db20bd5f85b9cd3e92c2c4f
root <6351@holbertonstudents.com>
2023-05-11 19:19:54 -0700

    fix

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -9,16 +9,16 @@
  */
 dlistint_t *add_node(dlistint_t **head, const int n)
 {
-	dlistint_t new_node;
+	dlistint_t *new_node;
 
-	new_node = malloc(sizeof(dlistint_t));
+	*new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 		return (NULL);
-	new_node.n = n;
-	new_node.next = *head;
-	new_node.prev = NULL;
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
 	if (head)
-		*head->prev = &new_node;
+		*head->prev = new_node;
 
-	return (&new_head);
+	return (new_head);
 }
