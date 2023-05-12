3211fda3ef3d0bf6ee843b2a2ed7439aa738d419
root <6351@holbertonstudents.com>
2023-05-11 19:49:33 -0700

    fix

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -24,7 +24,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if (*head)
 	{
 		tmp->next = new_node;
-		new_node->prev = *tmp;
+		new_node->prev = tmp;
 	} else
 		*head = new_node;
 
