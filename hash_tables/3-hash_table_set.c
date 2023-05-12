24a20f8d696dae0e49db7ba552966fbdccc5e0f0
root <6351@holbertonstudents.com>
2023-05-11 21:03:34 -0700

    hash table set

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -12,32 +12,19 @@ int hash_table_set(hash_table_t *ht, const unsigned char *key, const char *value
 	size_t index = key_index(key, ht->size);
 	hash_node_t *new_node;
 
-	if (!(ht || key || value))
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
 		return (0);
 
-	if (ht[index] != NULL)
-	{
-		ht[index].key = key;
-		ht[index].value = strdup(value);
-		if (!ht[index].value)
-			return (0);
-		ht[index].next = NULL;
-	} else
-	{
-		new_node = malloc(sizeof(hash_node_t));
-		if (!new_node)
-			return (0);
-		hash_note_t *current = ht[index]
-		while (current->next)
-			current = current->next;
-		current->next = &new_node;
-		new_node->key = key;
-		new_node->value = strdup(value);
-		if (!new_node->value)
-	
-			return (0);
-		new_node->next = NULL;
-	}
+	new_node->key = key;
+	new_node->value = strdup(value);
+	if (!value)
+		return (0)
 
+	if (ht->*array[index])
+		new_node->next = ht->*array[index];
+
+	*array[index] = new_node;
+		
 	return (1);
 }
