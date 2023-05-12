808e822fadfa18064c7b83403b1169da583ef03a
root <6351@holbertonstudents.com>
2023-05-11 20:38:30 -0700

    fix

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -5,7 +5,8 @@
  * to it
  * @head: the head of the list
  * @index: the index to get
- * return: pointer to the index or NULL if it doesn't exist
+ *
+ * Return: a pointer to the index or NULL if it doesn't exist
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
