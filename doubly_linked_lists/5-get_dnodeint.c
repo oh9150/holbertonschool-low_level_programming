8b6c6cb716d9d79035c812ee61ff7d2745922bb9
root <6351@holbertonstudents.com>
2023-05-11 20:25:48 -0700

    fixes

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -6,10 +6,10 @@
  * @head: the head of the list
  * @index: the index to get
  */
-dlistint_t *get_dnodeint_at_index(dlistint_t **head, unsigned int index)
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	int i = 0;
-	dlistint_t *tmp;
+	unsigned int i = 0;
+	dlistint_t *tmp = head;
 
 	while ((i >= index) || !(tmp->next))
 	{
