015fa91398e571cf0f0df15c90ae9971588f41bc
root <6351@holbertonstudents.com>
2023-05-16 20:23:05 -0700

    fix

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -20,10 +20,8 @@ int linear_search(int *array, size_t size, int value)
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 
 		if (array[i] == value)
-		{
-			printf("Found %d at index: %lu\n", value, i);
 			return (i);
-		}
+
 	}
 	return (-1);
 }
--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -1,4 +1,5 @@
 #ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
 
 #include <stdlib.h>
 #include <stdio.h>
