7f82fc6c0dd61a96fb38c667af36016391ab4c2d
root <6351@holbertonstudents.com>
2023-05-21 19:08:47 -0700

    minor fix.

--- search_algorithms/100-jump.c
+++ search_algorithms/100-jump.c
@@ -30,8 +30,7 @@ int jump_search(int *array, size_t size, int value)
 
 	while (i <= j)
 	{
-		printf("Value checked array[%d] = [%d]\n", i, array[i])
-			printf("Value checked array[%d] = [%d]\n", i, array[i]);;
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 
