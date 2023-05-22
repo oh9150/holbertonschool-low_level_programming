7de45d1360c32406b8620bc4bf7f33b7cee6a228
root <6351@holbertonstudents.com>
2023-05-21 19:06:03 -0700

    THIS is itgit add .git add .git add .!

--- search_algorithms/100-jump.c
+++ search_algorithms/100-jump.c
@@ -30,6 +30,9 @@ int jump_search(int *array, size_t size, int value)
 
 	while (i <= j)
 	{
+		if (array[i] == value)
+			return (i);
+
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		i++;
 	}
