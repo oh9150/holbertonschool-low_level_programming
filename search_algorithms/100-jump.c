790ac7267f76d4872ae48006437125a5545cecbb
root <6351@holbertonstudents.com>
2023-05-21 19:07:57 -0700

    linear search.

--- search_algorithms/100-jump.c
+++ search_algorithms/100-jump.c
@@ -30,10 +30,11 @@ int jump_search(int *array, size_t size, int value)
 
 	while (i <= j)
 	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i])
+			printf("Value checked array[%d] = [%d]\n", i, array[i]);;
 		if (array[i] == value)
 			return (i);
 
-		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		i++;
 	}
 
