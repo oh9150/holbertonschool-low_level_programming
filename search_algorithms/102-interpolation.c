25e94b5a86c1cda3f9fcf6d1c517b23736881ac5
root <6351@holbertonstudents.com>
2023-05-21 20:31:44 -0700

    interpolation search

---
+++ search_algorithms/102-interpolation.c
@@ -0,0 +1,32 @@
+#include "search_algos.h"
+
+/**
+ * interpolation_search - searches for @value in an array of integers @array
+ * using the interpolation search algorithm
+ * @array: the array to search in
+ * @size: the size of @array
+ * @value: the value to look for
+ * Return: the index at which @value is located, or -1 if it's not found
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	int high = size - 1, low = 0, pos;
+
+	while ((arr[high] != arr[low]) && (key >= arr[low]) && (key <= arr[high]))
+	{
+		pos = low + (((double)(high - low) / (array[high] - array[low])) *
+				(value - array[low]));
+		printf("Value checked array[%d] = [%d]", pos, arr[pos]);
+		if (arr[pos] < value)
+			low = pos + 1;
+		else if (value < arr[pos])
+			high = pos - 1;
+		else
+			return (pos);
+	}
+
+	if (key == arr[low])
+		return (low);
+
+	return (-1);
+}
