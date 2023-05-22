a7841c4ec6bbc0d06ef042180b76e60a07404afe
root <6351@holbertonstudents.com>
2023-05-21 20:38:59 -0700

    NULL case and out of range fix

--- search_algorithms/102-interpolation.c
+++ search_algorithms/102-interpolation.c
@@ -11,6 +11,9 @@
 int interpolation_search(int *array, size_t size, int value)
 {
 	int high = size - 1, low = 0, pos;
+	
+	if (!array)
+		return (-1);
 
 	while ((array[high] != array[low]) && (value >= array[low]) &&
 			(value <= array[high]))
@@ -29,5 +32,6 @@ int interpolation_search(int *array, size_t size, int value)
 	if (value == array[low])
 		return (low);
 
+	printf("Value checked array[%d] is out of range\n", pos);
 	return (-1);
 }
